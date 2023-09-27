8c1da149c81408afbadf4c587bd9589ccab474fa
BreElle Wells <breellejordyn@gmail.com>
2023-09-27 14:25:43 -0600

    render all post data

--- server/seed.py
+++ server/seed.py
@@ -12,10 +12,11 @@ from models import db, Post, User, Comment, Tag
 
 fake = Faker()
 
-tag1 = Tag(name='javascript')
-tag2 = Tag(name='python')
-tag3 = Tag(name='ruby')
-tag4 = Tag(name='react')
+tag1 = Tag(name="javascript")
+tag2 = Tag(name="python")
+tag3 = Tag(name="ruby")
+tag4 = Tag(name="react")
+
 
 def create_users():
     users = []
@@ -26,54 +27,57 @@ def create_users():
             name = fake.name()
         name_list.append(name)
 
-        s = User(name=name,)
+        s = User(
+            name=name,
+        )
         users.append(s)
 
     return users
 
-    
+
 def create_posts():
     posts = []
     tags = [tag1, tag2, tag3, tag4]
     for n in range(20):
         p = Post(
             title=f"random title no: {n+1}",
-            body = fake.text(),
-            user_id = rc([user.id for user in users])
+            body=fake.text(),
+            user_id=rc([user.id for user in users]),
         )
-            
+
+        p.tags.append(tags[randint(0, 3)])
         p.tags.append(tags[randint(0, 3)])
         posts.append(p)
 
     return posts
 
+
 def create_comments():
     comments = []
     for n in range(20):
         c = Comment(
-            content = fake.text(),
-            user_id = rc([user.id for user in users]),
-            post_id = rc([post.id for post in posts])
+            content=fake.text(),
+            user_id=rc([user.id for user in users]),
+            post_id=rc([post.id for post in posts]),
         )
         comments.append(c)
 
     return comments
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fake = Faker()
     with app.app_context():
         print("Clearing db...")
         db.drop_all()
-        
+
         print("Creating tables...")
         db.create_all()
-        
+
         print("Starting seed...")
 
-
         print("Seeding tags...")
-        
+
         db.session.add_all([tag1, tag2, tag3, tag4])
         db.session.commit()
 
@@ -81,7 +85,7 @@ if __name__ == '__main__':
         users = create_users()
         db.session.add_all(users)
         db.session.commit()
-        
+
         print("Seeding posts...")
         posts = create_posts()
         db.session.add_all(posts)
